913625ac1d1a2ebccc6ec1e73619ad3ed9f6900e
Gastòn Villanueva <gastivillanueva31@gmail.com>
2023-07-11 20:31:26 -0300

    version 2.0

--- tienda/urls.py
+++ tienda/urls.py
@@ -1,3 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
+# Resto de tus URLs...
+
+urlpatterns = [
+    # ...
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+
 
 from django.urls import path
 
